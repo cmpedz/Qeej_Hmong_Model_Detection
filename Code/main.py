fc5e19d585982b3af9cc90179136cffb9f9b4af9
Hung mnk <thekingcrazycraft@gmail.com>
2026-02-11 10:25:08 +0700

    adjust model

--- Code/main.py
+++ Code/main.py
@@ -1,25 +1,110 @@
 
 import numpy as np
-
-#preprocessing 
-fs = 48000
-
-window_size = int(0.01 * fs)
-
-def split_windows(signal, window_size):
-    return [
-        signal[i:i+window_size]
-        for i in range(0, len(signal) - window_size, window_size)
-    ]
-
-def extract_dft(window):
-    spectrum = np.abs(np.fft.rfft(window))
-    spectrum = spectrum / np.max(spectrum) 
-    return spectrum
-
-def suppress_low_amplitudes(spec, threshold_db=50):
-    I_threshold = 10 ** (-threshold_db / 20)
-    spec[spec < I_threshold] *= 0.2
-    return spec
+from module.DataModule import DataModule
+from sklearn.preprocessing import LabelEncoder
+from tensorflow.keras.utils import to_categorical
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Conv1D, MaxPooling1D
+from tensorflow.keras.layers import Dense, Dropout, Flatten
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import classification_report
+from sklearn.metrics import confusion_matrix, ConfusionMatrixDisplay
+import matplotlib.pyplot as plt
+from sklearn.utils.class_weight import compute_class_weight
 
 #label data
+audio_dir = "../Data/audio/Khèn 1/Đơn_ống"
+labels_dir = "../Data/labels/Khèn 1/Đơn_ống"
+dataModule = DataModule()
+X, y = dataModule.build_dataset(audio_dir, labels_dir)
+
+#label encoding
+le = LabelEncoder()
+y_int = le.fit_transform(y)
+y_cat = to_categorical(y_int)
+
+class_names = le.classes_
+
+
+#set up model
+model = Sequential([
+    Conv1D(32, kernel_size=5, activation='relu',
+           input_shape=(X.shape[1], 1)),
+    MaxPooling1D(2),
+    Dropout(0.25),
+
+    Conv1D(64, kernel_size=5, activation='relu'),
+    MaxPooling1D(2),
+    Dropout(0.25),
+
+    Flatten(),
+    Dense(128, activation='relu'),
+    Dropout(0.5),
+
+    Dense(y_cat.shape[1], activation='softmax')
+])
+
+model.compile(
+    optimizer='adadelta',
+    loss='categorical_crossentropy',
+    metrics=['accuracy']
+)
+
+#trainning model
+
+X_train, X_temp, y_train, y_temp = train_test_split(
+    X, y_cat, test_size=0.2, stratify=y_int)
+
+X_val, X_test, y_val, y_test = train_test_split(
+    X_temp, y_temp, test_size=0.5)
+
+y_train_int = np.argmax(y_train, axis=1)
+print("check y train int:", y_train_int)
+
+class_weights = compute_class_weight(
+    class_weight="balanced",
+    classes=np.unique(y_train_int),
+    y=y_train_int
+)
+
+class_weight_dict = dict(enumerate(class_weights))
+
+print("check class weight dict:", class_weight_dict)
+
+model.fit(
+    X_train[..., np.newaxis],
+    y_train,
+    validation_data=(X_val[..., np.newaxis], y_val),
+    class_weight=class_weight_dict,
+    epochs=30,
+    batch_size=100
+)
+
+#assess model
+y_pred = model.predict(X_test[..., np.newaxis])
+y_pred = np.argmax(y_pred, axis=1)
+y_true = np.argmax(y_test, axis=1)
+
+print("check y pred:", y_pred)
+print("check y true:", y_true)
+
+#F1 score, ...
+print(classification_report(
+    y_true, y_pred,
+    target_names=class_names
+))
+
+#confuse matrix
+cm = confusion_matrix(y_true, y_pred)
+
+fig, ax = plt.subplots(figsize=(18, 16))
+disp = ConfusionMatrixDisplay(
+    confusion_matrix=cm,
+    display_labels=class_names  
+)
+disp.plot(ax=ax)
+
+plt.xlabel("Predicted label")
+plt.ylabel("True label")
+plt.title("Confusion Matrix (10 ms windows)")
+plt.show()
---
+++ Code/module/DataModule.py
@@ -0,0 +1,92 @@
+import numpy as np
+import pandas as pd
+import librosa
+from pathlib import Path
+
+class DataModule:
+
+    def __extract_dft(self, window):
+        spectrum = np.abs(np.fft.rfft(window))
+        spectrum = spectrum / np.max(spectrum) 
+        return spectrum
+
+    def __suppress_low_amplitudes(self, spec, threshold_db=50):
+        I_threshold = 10 ** (-threshold_db / 20)
+        spec[spec < I_threshold] *= 0.2
+        return spec
+    def __get_spectrum_of_window(self, window):
+
+        spectrum = self.__extract_dft(window)
+
+        #preprocessing
+        standard_spectrum = spectrum#self.__suppress_low_amplitudes(spectrum)
+
+        return standard_spectrum
+    
+    def __process_one_file_audio(
+        self,
+        audio_path,
+        csv_path,
+        sample_rate=48000,
+        window_size=0.01
+    ):
+        #load audio
+        audio, sr = librosa.load(audio_path, sr=sample_rate, mono=True)
+
+        samples_per_window = int(sample_rate * window_size)
+
+        # Load CSV
+        df = pd.read_csv(csv_path)
+        X = []
+        y = []
+
+        # preparing data 
+        for _, row in df.iterrows():
+            start_sample = int(row["start_time"] * sample_rate)
+            end_sample = start_sample + samples_per_window
+
+            if end_sample > len(audio):
+                continue
+
+            window = audio[start_sample:end_sample]
+            spectrum = self.__get_spectrum_of_window(window)
+
+            X.append(spectrum)
+            y.append(row["label"])
+
+        return np.array(X), np.array(y)
+    
+    def build_dataset(self, audio_dir, csv_dir):
+        audio_dir = Path(audio_dir)
+        csv_dir = Path(csv_dir)
+
+        X_all = []
+        y_all = []
+
+        for audio_file in audio_dir.glob("*.wav"):
+            csv_file = csv_dir / (audio_file.stem + ".csv")
+
+            if not csv_file.exists():
+                print(f"unable to find csv file for {audio_file.name}")
+                continue
+
+            X, y = self.__process_one_file_audio(audio_file, csv_file)
+
+            X_all.append(X)
+            y_all.append(y)
+
+        X_all = np.vstack(X_all)
+        y_all = np.hstack(y_all)
+
+        return X_all, y_all
+
+
+
+if __name__ == "__main__":
+    audio_dir = "../Data/audio/Khèn 1/Đơn_ống"
+    labels_dir = "../Data/labels/Khèn 1/Đơn_ống"
+    dataModule = DataModule()
+    X, y = dataModule.build_dataset(audio_dir, labels_dir)
+    
+    print("X shape:", X.shape)
+    print("y shape:", y.shape)
